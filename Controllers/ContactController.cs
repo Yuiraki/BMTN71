59d5b1ff138520761409e1aa54805dd146716653
Yuiraki <lamvu0727@gmail.com>
2026-01-11 19:05:02 +0700

    Add files via upload

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using startup.Models;
+
+namespace startup.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly DataContext _context;
+
+        public ContactController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: /Contact
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // POST: /Contact
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(Contact model)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Contacts.Add(model);
+                _context.SaveChanges();
+
+                ViewBag.Success = "Gửi liên hệ thành công!";
+                ModelState.Clear();
+            }
+
+            return View();
+        }
+    }
+}
---
+++ Models/Contact.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace startup.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập tên")]
+        public string FullName { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập email")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập số điện thoại")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập nội dung")]
+        public string Message { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
